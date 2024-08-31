3a934acdbe605217bfb58520ec1d9152a8ce2fde
ViniHoffmann <124735617+VinciusHofs@users.noreply.github.com>
2024-08-30 22:02:46 -0300

    Add files via upload

---
+++ 30082024/coisa.py
@@ -0,0 +1,30 @@
+"""""
+num = int(input('informe um número: '))
+
+if num < 0:
+    num = num*-1
+
+print('o módulo do número é', num)
+"""
+
+"""
+import math
+n1 = int(input('Informe a nota 1: '))
+n2 = int(input('Informe a nota 2: '))
+n3 = int(input('Informe a nota 3: '))
+n4 = int(input('Informe a nota 4: '))
+
+notaFinal = (n1+n2+n3+n4)/4
+
+if notaFinal >= 5:
+    print('Sua média foi ', notaFinal, ' Você foi aprovado!')
+else:
+    print('Sua média foi ', notaFinal, ' Você reprovou!')
+"""
+
+
+import math
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe outro número: '))
+n3 = int(input('Informe mais um número: '))
+n4 = int(input('Informe o último número: '))
