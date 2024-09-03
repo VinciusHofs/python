4865622f5f7550189e9dac9ea704586714859be9
ViniHoffmann <124735617+VinciusHofs@users.noreply.github.com>
2024-09-02 22:07:08 -0300

    Add files via upload

---
+++ 02092024/atv.py
@@ -0,0 +1,71 @@
+"""
+num = int(input('Informe um número: '))
+if num%2==0 and num%3==0:
+    print('O número', num, 'é divisível por 2 e 3')
+else: 
+    print('O número', num, 'não é divisível por 2 e 3')
+
+num2 = int(input('Informe um número: '))
+if num2%2==0 and num2%3==0:
+    print('O número', num2, 'é divisível por 2 e 3')
+else: 
+    print('O número', num2, 'não é divisível por 2 e 3')
+
+num3 = int(input('Informe um número: '))
+if num3%2==0 and num3%3==0:
+    print('O número', num3, 'é divisível por 2 e 3')
+else: 
+    print('O número', num3, 'não é divisível por 2 e 3')
+
+num4 = int(input('Informe um número: '))
+if num4%2==0 and num4%3==0:
+    print('O número', num4, 'é divisível por 2 e 3')
+else: 
+    print('O número', num4, 'não é divisível por 2 e 3')
+"""
+
+"""
+num = int(input('Informe um número: '))
+num2 = int(input('Informe um número: '))
+num3 = int(input('Informe um número: '))
+num4 = int(input('Informe um número: '))
+
+if num%2==0 or num%3==0:
+    print('O número', num, 'é divisível por 2 ou 3')
+else: 
+    print('O número', num, 'não é divisível por 2 ou 3')
+
+if num2%2==0 or num2%3==0:
+    print('O número', num2, 'é divisível por 2 ou 3')
+else: 
+    print('O número', num2, 'não é divisível por 2 ou 3')
+
+if num3%2==0 or num3%3==0:
+    print('O número', num3, 'é divisível por 2 ou 3')
+else: 
+    print('O número', num3, 'não é divisível por 2 ou 3')
+
+if num4%2==0 or num4%3==0:
+    print('O número', num4, 'é divisível por 2 ou 3')
+else: 
+    print('O número', num4, 'não é divisível por 2 ou 3')
+"""
+
+"""
+idade = int(input('Informe a sua idade: '))
+
+if idade>=5 and idade<=7:
+    print('Infantil A')
+elif idade>=8 and idade<=10:
+    print('Infantil B')
+elif idade>=11 and idade<=13:
+    print('Juvenil A')
+elif idade>=14 and idade<=17:
+    print('Juvnil B')
+elif idade>=18 and idade<=60:
+    print('Adulto')
+elif idade>60:
+    print('Senior')
+elif idade<5:
+    print('Categoria inexistente')
+"""
