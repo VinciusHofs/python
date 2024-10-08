6230a13fce0415329995ad0c5cbe5d72b9323602
ViniHoffmann <124735617+VinciusHofs@users.noreply.github.com>
2024-10-07 22:06:34 -0300

    Add files via upload

---
+++ 07102024/repeticao.py
@@ -0,0 +1,28 @@
+"""
+i = 0
+
+while i < 5:
+    x = int(input("informe um número: "))
+    i = i + 1 #i += 1
+"""
+
+"""
+sair = 'S'
+while sair != 'N':
+    sair = input("deseja continuar S (Sim) ou N (Não)")
+"""
+
+"""
+i = 1
+resultado = 0
+while i <= 5:
+    #print(i)
+    i += 1
+    resultado = resultado + i
+print(resultado)
+"""
+
+i = 7
+while i < 1000:
+    print(i)
+    i += 7
